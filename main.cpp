078cd6625aaa090263539270251ecb56e18f2bed
Simone Vacondio <vacondio@proton.me>
2024-11-27 21:52:21 +0100

    Jot down pseudo-code for main

--- main.cpp
+++ main.cpp
@@ -11,9 +11,27 @@ int main()
   // Puzzle puzzle { InitType{horizSwap} };
   // Puzzle puzzle { InitType{vertSwap} };
   // Puzzle puzzle { InitType{randomOrder} };
-  
+
   puzzle.print_boxes();
 
   std::cout << "\n" << "Hello world!\n";
   return 0;
+
+  // Pseudo-code for the game
+  // print welcome message
+  // loop : while(true)
+  //   print puzzle : cout << puzzle;
+  //   read move from user : cin >> puzzle;
+  //   until puzzle is solved : if (puzzle) break
+  // endloop
+  // congratulate user and quit
+  //
+  // that'll roughly translate to:
+  // std::cout << "Welcome to 15puzzle:";
+  // while(true)
+  //   {
+  //     cout << puzzle;
+  //     cin  >> puzzle; //needs input validity checks
+  //     if (puzzle) break;
+  //   }
 }
